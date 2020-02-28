d2b8e4c2172db91db711c58b24e8524ea369f5d5
wlm2012 <541253785@qq.com>
2020-02-28 20:39:21 +0800

    init sql

--- demo/sql/init.sql
+++ demo/sql/init.sql
@@ -3,8 +3,8 @@ CREATE TABLE `t_student` (
   `code` varchar(20) DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
   `nianji` varchar(2) DEFAULT NULL,
-  `banji` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+  `banji` varchar(3) CHARACTER SET utf8mb4  DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 
@@ -12,14 +12,14 @@ CREATE TABLE `t_clazz` (
   `clazz` varchar(10) DEFAULT NULL,
   `banji` varchar(3) DEFAULT NULL,
   `nianji` varchar(2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 
 CREATE TABLE `t_teacher` (
   `name` varchar(40) DEFAULT NULL,
   `kemu` varchar(3) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
 
 CREATE TABLE `t_tiku` (
@@ -27,7 +27,7 @@ CREATE TABLE `t_tiku` (
   `leixing` varchar(2) DEFAULT NULL,
   `daan` varchar(100) DEFAULT NULL,
   `mekubianhao` varchar(3) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 
@@ -35,19 +35,19 @@ CREATE TABLE `t_shijuan` (
   `bianhao` varchar(10) DEFAULT NULL,
   `timubianhao` varchar(10) DEFAULT NULL,
   `clazz` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 
 CREATE TABLE `t_kemu` (
   `bianhao` varchar(3) DEFAULT NULL,
   `miaoshu` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
 
 
 CREATE TABLE `t_chengji` (
   `xueshengbianhao` varchar(20) DEFAULT NULL,
   `chengji` varchar(3) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
